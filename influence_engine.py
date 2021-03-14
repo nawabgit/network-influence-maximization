ff79c1bbd20732e56c7aad313e68251d715935a3
Nawab <nawab.mir14@gmail.com>
2021-03-14 22:56:27 +0000

    added network drawing method

--- influence_engine.py
+++ influence_engine.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import networkx as nx
+from matplotlib import pyplot as plt
 
 import random
 random.seed(10)
@@ -55,8 +57,9 @@ class InfluenceEngine:
 
             for neighbour in neighbours:
                 # Attempt, if the neighbour hasn't already been activated
-                if not neighbour['active']:
+                if not G_t.nodes[neighbour]['active']:
                     p = G_t[node][neighbour]['p']
+                    G_t[node][neighbour]['live'] = True
                     outcome = random.uniform(0, 1)
 
                     # Store the neighbour for activation if within CDF
@@ -64,9 +67,22 @@ class InfluenceEngine:
                         activations.append(neighbour)
 
         # Activate successful nodes
-        self.activate_nodes(activations)
+        self.activate_nodes(t, activations)
 
         return activations
+
+    def draw_graph(self, t):
+        """
+        Displays the current state of the network including activated nodes and live edges
+        at current time step
+
+        :param t:
+        """
+        color_map = ['green' if self.G[t].nodes[node]['active'] else 'red' for node in self.G[t].nodes()]
+        edge_color_map = ['red' if self.G[t][e1][e2]['live'] else 'black' for e1, e2 in self.G[t].edges]
+
+        nx.draw_spring(self.G[t], node_color=color_map, edge_color=edge_color_map, with_labels=True)
+        plt.show()
 
     def simulate_independent_cascade(self, initial_nodes):
         """
@@ -91,8 +107,9 @@ class InfluenceEngine:
             self.activate_nodes(t, initial_nodes[t])
             # Prime seed nodes for the next time step
             last_activated.extend(initial_nodes[t])
+            self.draw_graph(t)
 
         # Retrieve all activated nodes in the final graph instance
-        influence = len([x for x, y in self.G[-1].nodes(data=True) if y['active'] == True ])
+        influence = len([x for x, y in self.G[-1].nodes(data=True) if y['active']])
 
         return influence
